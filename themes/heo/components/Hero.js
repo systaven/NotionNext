973937fb3e8ee5b4707494c6fd780c9d6034fb7a
stevenyang770406 <stevenyang770406@gmail.com>
2026-02-27 00:02:57 +0000

    随便逛逛修改

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -102,11 +102,11 @@ function Banner(props) {
         id='banner-cover'
         style={{ backdropFilter: 'blur(15px)' }}
         className={
-          'z-20 rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#FFD9EF] dark:bg-[#701E49] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
+          'z-20 rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#FFD9EF] dark:bg-[#201f26] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
         }>
         <div className='ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in'>
-          <div className='text-7xl text-white font-extrabold'>{coverTitle}</div>
-          <div className='-ml-3 text-gray-300'>
+          <div className='text-7xl text-[#333] dark:text-white font-extrabold'>{coverTitle}</div>
+          <div className='-ml-3 text-[#666] dark:text-gray-300'>
             <ArrowSmallRight className={'w-24 h-24 stroke-2'} />
           </div>
         </div>
